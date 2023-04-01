4e5af79e4fa20dbb0621df9129c434ad353d2d56
Microbacteia <79184880+mizmo-cheese@users.noreply.github.com>
2023-04-01 13:42:03 +0700

    Images folder, settings py
    Settings class
    
    img - alien, ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,35 @@
 import sys
-
 import pygame
+
+from settings import Settings
 
 
 class AlienInvasion:
 
     def __init__(self):
-        
+        #инит настройки игры
         pygame.init()
-        
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+
+        self.bgcolor = (230, 230, 230)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_widght, self.settings.screen_height))
+        pygame.display.set_caption("Заголовок")
 
 
 
     def run_game(self):
-        
-        while True:
+     while True:
+             #нажатия контроль
             for event in pygame.event.get():
+                #если закрыть окно
                 if event.type == pygame.QUIT:
                     sys.exit()
    
+          # Перерисоывается экран
+            self.screen.fill(self.bgcolor)
 
+         # Последний отрисованый экран
             pygame.display.flip()
 
 if __name__ == '__main__':
